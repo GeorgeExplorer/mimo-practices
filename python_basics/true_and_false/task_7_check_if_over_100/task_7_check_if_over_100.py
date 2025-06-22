a8c4be8088adc1fffc9bd840a4951ebf1e509cb1
GeorgeExplorer <99310998+GeorgeExplorer@users.noreply.github.com>
2025-06-22 16:17:55 +0000

    Resolve task 7: task_7_check_if_over_100

---
+++ python_basics/true_and_false/task_7_check_if_over_100/task_7_check_if_over_100.py
@@ -0,0 +1,7 @@
+input_number = int(float(input ("Enter the number you want: ")))
+result_checking_numbers = input_number > 100
+result_comparing_numbers = input_number == 100
+print(f"Is input number greater than 100? Result: {result_checking_numbers}")
+print(f"Is the number equal to 100? Result: {result_comparing_numbers}")
+
+
